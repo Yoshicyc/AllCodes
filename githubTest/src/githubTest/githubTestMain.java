6a72669d243065a5b7009e0cbd03de27a720e5f5
Yoshi <Yoshi@172.16.67.27>
2018-09-09 16:04:53 +0800

    all codes

---
+++ githubTest/src/githubTest/githubTestMain.java
@@ -0,0 +1,11 @@
+package githubTest;
+
+public class githubTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Github");
+		System.out.println("Upload test");
+	}
+
+}
